d8e8f4cced089e0d10a57c8033090eece006b434
Brenno Prettyman <brennoprettyman@gmail.com>
2023-04-19 10:17:29 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+    
+    Console.Clear();
+    int Faces, Rolagem;
+
+    Console.Write("Insira Quantidade de Faces do dado: ");
+    Faces = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine();
+
+    Rolagem = RandomNumberGenerator.GetInt32(1, Faces + 1);
+
+    Console.WriteLine($"O número da rolagem é: {Rolagem}"); 
